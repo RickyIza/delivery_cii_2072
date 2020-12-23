e54fde8e938447c047d50cf96edc179271d69ba0
Ricky Iza <rck.ikbm@hotmail.com>
2020-12-22 21:07:55 -0500

    Delivery

---
+++ delivery-CII/src/main/java/com/cii/delivery/DeliveryCiiApplication.java
@@ -0,0 +1,13 @@
+package com.cii.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryCiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryCiiApplication.class, args);
+	}
+
+}
---
+++ delivery-CII/src/test/java/com/cii/delivery/DeliveryCiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cii.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryCiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
